9bb91f2e17f1b9b4cc0605f620a241915ae2f824
YY <550872569@qq.com>
2018-09-05 16:52:09 +0800

    add dir

---
+++ CKYSTemplate/CKYSTemplate/AppDelegate/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  CKYSTemplate
+//
+//  Created by Yan on 2018/7/5.
+//  Copyright © 2018年 YY. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [[ViewController alloc]init];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+@end
