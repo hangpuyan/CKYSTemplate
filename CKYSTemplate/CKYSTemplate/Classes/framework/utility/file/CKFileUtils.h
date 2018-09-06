9ebb4a881425d01eaad693fd292ec0036ce2aced
YY <550872569@qq.com>
2018-09-06 11:48:19 +0800

    add CKFileUtils

---
+++ CKYSTemplate/CKYSTemplate/Classes/framework/utility/file/CKFileUtils.h
@@ -0,0 +1,21 @@
+//
+//  CKFileUtils.h
+//  CKYSTemplate
+//
+//  Created by Yan on 2018/9/6.
+//  Copyright © 2018年 YY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKFileUtils : NSObject
+
++ (BOOL)pathExists:(NSString *)path isDir:(BOOL *)isDir;
+
++ (void)createDirectoryAtPath:(NSString *)dirPath;
++ (void)createFileAtPath:(NSString *)filePath;
+
++ (BOOL)moveSrcPath:(NSString *)srcPath toDestPath:(NSString *)destPath overWrite:(bool)overWrite;
++ (BOOL)deletePath:(NSString *)path;
+
+@end
