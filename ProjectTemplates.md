51232a5209b56930594d7a597109aedecfaf2960
YY <550872569@qq.com>
2018-09-07 16:54:30 +0800

    add ProjectTemplates

---
+++ ProjectTemplates.md
@@ -0,0 +1,3 @@
+Xcode系统模板的路径是/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/，文件夹里面有文件模板File Templates和工程模板Project Templates，分别对应创建文件时的选项和创建工程时的选项：
+
+1. ___FILEBASENAME___  ViewController Templates 
---
+++ ___FILEBASENAME___.m
@@ -0,0 +1,51 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Life
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+#pragma mark - InitUI
+
+#pragma mark - Delegate
+
+#pragma mark - Action
+
+#pragma mark - Private
+
+#pragma mark - Getter
+
+#pragma mark - Setter
+
+#pragma mark -
+
+- (void)dealloc {
+    NSLog(@"self:%@",self);
+}
+
+@end
